b02044271d90f1452d677b16412fa7acd56ab589
NoheliGT <noheligar688@gmail.com>
2022-05-02 17:19:20 -0500

    9

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ require('ts-node').register({ transpileOnly: true });
 // Make sure data folder exists
 const fs = require('fs');
 fs.mkdirSync('./data', { recursive: true });
+var express = require("express");
+var app = express();
+
+
+app.set("port", process.env.PORT || 5000);
+
+app
+  .get("/", function (request, response) {
+    var result = "App is running";
+    response.send(result);
+  })
+  .listen(app.get("port"), function () {
+    console.log(
+      "App is running, server is listening on port ",
+      app.get("port")
+    );
+  });
 
 // Utils
 const { logError } = require('./utils/log');
